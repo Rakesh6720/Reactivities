2774c3de19ab36f37368f9d914b6c74e861f421c
Rakesh Baruah <rbaruah@gmail.com>
2021-11-23 17:25:22 -0600

    setup semantic-ui in app.tsx

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
@@ -15,9 +14,8 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div>
       <Header as="h2" icon="users" content="Reactivities" />
-
       <List>
         {activities.map((activity: any) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
